b08936418b49aaf9d5684fbecc6fdd0f953af782
HToy91 <jfvargasfigueroa@email.neit.edu>
2026-04-08 11:55:02 -0400

    Add Lab2 Rock-Paper-Scissors game and restructure
    Rename project folder from W2/untitled to W2/Lab2 and update IDE module reference. Add a simple CLI Rock-Paper-Scissors implementation: Game.java (MainMenu, PlayGame, DetermineWinner), Player.java (player model with moves), and Main.java (entrypoint). Also add .DS_Store files created by the OS.

---
+++ W2/Lab2/src/Lab2/Game.java
@@ -0,0 +1,69 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class Game {
+    public static void MainMenu (Scanner scanner) {
+        System.out.println("Would you like to play a game (Y/N)?");
+        String answer = scanner.nextLine();
+
+        if (answer.equalsIgnoreCase("n")) {
+            System.out.println("Thank you for playing!");
+            return;
+        }
+
+        System.out.println("Please enter your name");
+        String name = scanner.nextLine();
+
+        Player player = new Player(name, 0);
+        Player computer = new Player("Korg", 0);
+
+        while (answer.equalsIgnoreCase("y")) {
+            PlayGame(name, player, computer, scanner);
+
+            System.out.println("Would you like to play again (Y/N)?");
+            answer = scanner.nextLine();
+        }
+        System.out.println("Final Scores");
+        System.out.print(player.name + ": " + player.score + " | ");
+        System.out.println(computer.name + ": " + computer.score);
+    }
+
+    private static void PlayGame(String name, Player player, Player computer, Scanner scanner) {
+
+        System.out.println("Welcome " + player.name + " to the game of Rock, Paper, Scissors!");
+        System.out.println("Please enter your choice (rock, paper, scissors)");
+        String playerChoice = scanner.nextLine();
+
+        String computerChoice = computer.moves[(int) (Math.random() * 3)];
+        System.out.println("The computer chose " + computerChoice);
+
+        DetermineWinner(playerChoice, computerChoice, player, computer);
+
+        System.out.println(player.name + "'s score: " + player.score);
+        System.out.println(computer.name + "'s score: " + computer.score);
+    }
+
+    private static void DetermineWinner(String playerChoice, String computerChoice, Player player, Player computer) {
+        String result = playerChoice.toLowerCase() + "-" + computerChoice.toLowerCase();
+
+        switch (result) {
+            case "rock-rock":
+            case "paper-paper":
+            case "scissors-scissors":
+                System.out.println("It's a tie!");
+                break;
+
+            case "rock-scissors":
+            case "paper-rock":
+            case "scissors-paper":
+                System.out.println("Congratulations " + player.name + ", you win!");
+                player.score++;
+                break;
+
+            default:
+                System.out.println("Sorry " + player.name + ", " + computer.name +" wins!");
+                computer.score++;
+        }
+    }
+}
---
+++ W2/Lab2/src/Lab2/Player.java
@@ -0,0 +1,13 @@
+package Lab2;
+
+public class Player {
+    String name;
+    int score;
+
+    String[] moves = {"rock", "paper", "scissors"};
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
